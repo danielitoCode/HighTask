10fa7b1c79e3e3c35818656d576927a4a60caa2b
Danielito1996 <daniel.imbert96@hotmail.com>
2025-12-25 17:01:06 -0600

    Jobs feacture and DI config

--- composeApp/src/jsMain/kotlin/com/elitec/hightask/infraestructure/log/isDebug.js.kt
+++ composeApp/src/jsMain/kotlin/com/elitec/hightask/infraestructure/log/isDebug.js.kt
@@ -1,7 +1,5 @@
 package com.elitec.hightask.infraestructure.log
 
-import androidx.compose.runtime.ComposeCompilerApi
-
 actual fun isDebug(): Boolean {
     return true
 }
--- composeApp/src/webMain/kotlin/com/elitec/hightask/feacture/job/presenntation/JobsScreen.kt
+++ composeApp/src/webMain/kotlin/com/elitec/hightask/feacture/job/presenntation/JobsScreen.kt
@@ -3,12 +3,10 @@ package com.elitec.hightask.feacture.job.presenntation
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 
