cf94662558e7becb6ccc126c5c757acb72661faa
Danielito1996 <daniel.imbert96@hotmail.com>
2025-12-25 17:00:16 -0600

    Jobs feacture and DI config

--- composeApp/src/webMain/kotlin/com/elitec/hightask/infraestructure/di/networkModules.kt
+++ composeApp/src/webMain/kotlin/com/elitec/hightask/infraestructure/di/networkModules.kt
@@ -1,7 +1,5 @@
 package com.elitec.hightask.infraestructure.di
 
-import io.ktor.client.HttpClient
 import org.koin.core.module.Module
-import org.koin.dsl.module
 
 expect fun networkModules(): Module
