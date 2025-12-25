c2ff59e91c7c4be2ddd66005f33d85e82d21367c
Danielito1996 <daniel.imbert96@hotmail.com>
2025-12-25 16:59:39 -0600

    Jobs feacture and DI config

--- composeApp/src/jsMain/kotlin/com/elitec/hightask/infraestructure/log/isDebug.js.kt
+++ composeApp/src/jsMain/kotlin/com/elitec/hightask/infraestructure/log/isDebug.js.kt
@@ -1,5 +1,7 @@
 package com.elitec.hightask.infraestructure.log
 
+import androidx.compose.runtime.ComposeCompilerApi
+
 actual fun isDebug(): Boolean {
-    TODO("Not yet implemented")
+    return true
 }
